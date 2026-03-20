680e52ba6cda7b071aa85e565484d3742a43391d
Guillaume de Maricourt <ahz45bny@gmail.com>
2026-03-20 10:49:28 +0100

    feat: implement script errors
    This commit adds errors.
    Errors are based on architecture : "docs/arch/arch.drawio"
    Details can be found in "docs/utils/commmon_keys-consts".

---
+++ scripts/repeact-lib/error.sh
@@ -0,0 +1,25 @@
+# ==================== #
+# Scripts errors
+# ==================== #
+# Must follow "docs/utils/commmon_keys-consts"
+# Any update on this file shall be the mirror of previous file !
+# ==================== #
+
+readonly ERR_OK=0
+readonly ERR_SUDO=1
+readonly ERR_SANITY=2
+readonly ERR_FILE_NOT_FOUND=3
+readonly ERR_MOVE=4
+readonly ERR_EXECUTE_PERMISSION_DENIED=5
+readonly ERR_SYMLINK_DUPLICATE=6
+readonly ERR_SYMLINK_CREATE=7
+readonly ERR_SYS_UPDATE=8
+readonly ERR_DEP_UPDATE=9
+readonly ERR_BOOT_CONF_NOT_FOUND=10
+readonly ERR_TIMINGS_FORMAT=11
+readonly ERR_RECORD_IN_PROGRESS=12
+readonly ERR_RECORD_HW_UNAVAILABLE=13
+readonly ERR_NO_RECORD=14
+readonly ERR_PID_CORRUPTED=15
+readonly ERR_RECORD_FFMPEG_PROCESS_LOCKED=16
+readonly ERR_RECORD_V4L2_PROCESS_LOCKED=17
