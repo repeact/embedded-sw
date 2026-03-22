bd22186e597248015a39503d4cb80021eaa4f74c
Guillaume de Maricourt <ahz45bny@gmail.com>
2026-03-22 12:56:06 +0100

    feat: first step of arch implementation
    This commit implement the following fucntions:
    - sudo check
    - file deployement
    - execute permissions once deployed

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,9 +15,24 @@ source "$SCRIPT_DIR/errors.sh"
 source "$SCRIPT_DIR/const.sh"
 source "$SCRIPT_DIR/pix-common.sh"
 
-# Deploy files
+sudo-check
 
+# Deploy files
+log "info" "Deploying files to $PIX_DIR"
+mkdir -p "$PIX_DIR"
+
+for F in "${EXECUTABLES[@]}" "${SOURCES[@]}"; do
+    mv "$SCRIPT_DIR/$F" "$PIX_DIR/$F"
+done
 # Set execute permissions
+log "info" "Setting execute permissions"
+
+for F in "${EXECUTABLES[@]}"; do
+    if ! chmod +x "$PIX_DIR/$F"; then
+        log "err" "chmod +x failed: $PIX_DIR/$F"
+        exit "$ERR_EXECUTE_PERMISSION_DENIED"
+    fi
+done
 
 # Create symlinks
 
