cb6c600ed7f5b32fddb552326a2d495b04798a3c
Guillaume de Maricourt <ahz45bny@gmail.com>
2026-03-22 12:21:57 +0100

    chore(const): add constants dependencies
    This commits adds the required constants used by install script such as
    the packages dep version, the DTO configuration. It also does setup the
    global install placeholders.

---
+++ scripts/repeact-lib/const.sh
@@ -0,0 +1,32 @@
+# Required project constants
+
+# Install paths
+PIX_DIR="/etc/pix"
+BIN_DIR="/usr/local/bin"
+CONFIG_FILE="/boot/firmware/config.txt"
+
+# Pinned dependency versions
+# Hardcoded intentionally: simplified bug tracking
+PKG_V4L2_UTILS="v4l-utils=1.30.1*"
+PKG_FFMPEG="ffmpeg=7.1*"
+
+# Scripts deployed (to $PIX_DIR)
+# w/ chmod +x
+# w/ symlinks ($BIN_DIR)
+EXECUTABLES=(
+)
+
+# Required dependencies files
+SOURCES=(
+
+)
+
+# Required hardware config (DTO, in DT$CONFIG_FILE)
+SETTINGS=(
+	"camera_auto_detect=0"
+	"dtoverlay=tc358743"
+	"dtoverlay=cma,cma-128"
+	"gpu_mem=128"
+	"enable_uart=1"
+	"dtoverlay=disable-bt"
+)
