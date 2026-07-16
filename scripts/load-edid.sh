5bbd0969611c9a231d337240569a2c0282da234a
Guillaume de Maricourt <ahz45bny@gmail.com>
2026-07-16 22:22:46 +0200

    feat(tools): implement load-edid script

--- scripts/load-edid.sh
+++ scripts/load-edid.sh
@@ -9,6 +9,20 @@ source "/etc/repeact/lib/const"
 source "/etc/repeact/lib/common"
 
 load_edid() {
+    local edid_path="$CONFIG_DIR/$EDID_FILE"
+
+    log "info" "Loading EDID from $EDID_FILE"
+    if [[ ! -f $edid_path ]]; then
+        log "err" "EDID file not found: $EDID_FILE"
+        exit "$ERR_FILE_NOT_FOUND"
+    fi
+
+    if ! v4l2 -d "$VIDEO_DEVICE" --set-edid=file="$edid_path" > /dev/null; then
+        log "err" "Video device not found: $VIDEO_DEVICE"
+        exit "$ERR_VIDEO_DEVICE_NOT_FOUND"
+    fi
+
+    log "info" "EDID loaded successfully."
 }
 
 load_edid
