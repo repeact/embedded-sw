acf82e49a2b47f869396d86cfbfa1bad87605c94
Guillaume de Maricourt <ahz45bny@gmail.com>
2026-07-16 22:22:11 +0200

    chore(tools): create load-edid script code skeleton

---
+++ scripts/load-edid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# =============================================================================
+# HW bridge edid loader (video0)
+# =============================================================================
+set -euo pipefail
+
+source "/etc/repeact/lib/errors"
+source "/etc/repeact/lib/const"
+source "/etc/repeact/lib/common"
+
+load_edid() {
+}
+
+load_edid
+
+exit "$ERR_OK"
