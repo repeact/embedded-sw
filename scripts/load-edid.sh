5f0067b11701073dcc09b53c66d5f9bc38fa11d2
Guillaume de Maricourt <ahz45bny@gmail.com>
2026-07-23 00:10:53 +0200

    docs: improve load-edid script header

--- scripts/load-edid.sh
+++ scripts/load-edid.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 # =============================================================================
-# Hardware bridge setup: edid loader.
-# This script will be triggered by systemd/udev when HDMI to CSI bridge is available.
-# (TC358743, v4l2 video0 node)
+# Bridge     : HDMI to CSI bridge based on toshiba TC358743 (=> continued as TC9590XBG).
+# Device/sink: Device receiving video feed (current, alias "repeact").
+# EDID       : "Extended Display Identification Data".
+#              Sink video capabilities advertizer with some metadatas.
+# =============================================================================
+# Hardware bridge setup.
+# Load edid file into HDMI bridge.
+# 
+# This script is triggered by udev+systemd units when bridge is available (v4l2 video0 node).
+# EDID is loaded/stored into bridge memory without needing a source available (no cable connected).
 #
-# NOTE: This "script" failing is a fatal error and SHALL be handled (hence related service).
-# Failure means that the bridge is not linked to MCU: device hardware issue only (not user end).
+# WARNING: This script@service failing is a fatal error and SHALL be handled carefully.
+# A failure means that the bridge is not linked to the MCU (RPI here) which ALWAYS means
+# that hardware is broken (PCB and/or flexible cable).
+# This is why all of the package's script@services rely on this script@service success.
+#
+# Documentation reference:
+# https://toshiba.semicon-storage.com/info/TC358743XBG_datasheet_en_20260511.pdf?did=35655&prodName=TC358743XBG
+# https://ia801404.us.archive.org/3/items/CEA-861-B/CEA-861-B.pdf
+#
+# Tool references for building custom EDID:
+# https://edid.build/
+# https://thyge.github.io/edid-editor/
 # =============================================================================
 set -euo pipefail
 
