3689ca4e4dc896792803ff406b30aba3bfc716e5
Guillaume de Maricourt <ahz45bny@gmail.com>
2026-07-27 23:57:57 +0200

    feat(deploy): implement lock-src-signal service unit

--- scripts/src-avail-daemon.sh
+++ scripts/src-avail-daemon.sh
@@ -65,11 +65,9 @@ manage_service() {
 
     log "info" "Managing service depending on source avail/unavail for $VIDEO_DEVICE"
     if [[ "$signal_bit_value" -eq 0 ]]; then
-        # systemctl start "$DAEMON_SERVICE_CALLEE"
-        :
+        systemctl start "$DAEMON_SERVICE_CALLEE"
     else
-        # systemctl stop "$DAEMON_SERVICE_CALLEE"
-        :
+        systemctl stop "$DAEMON_SERVICE_CALLEE"
     fi
 
     exit "$ERR_OK"
